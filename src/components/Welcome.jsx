6ab62b5cb89d8a7577dbad844bf5c8ea9c724440
kemtay-rp <163384607+kemtay-rp@users.noreply.github.com>
2024-08-02 04:31:09 +0000

    useState and useEffect

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Meet the StarGazers</h1> 
+        
+      </>);
+  }
+};
+
+export default Welcome;
